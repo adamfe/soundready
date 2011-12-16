5dc6802b3223b2154ec159221a246b2ae1d8e49e
Danny Hertz <danny@twitter.com>
2011-12-15 16:59:19 -0800

    my first commit. woot.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Soundready::Application.config.secret_token = 'aa821c0bfed7aed213ff951f2baffb4eef831ed65ee87693a43a2f85d2ff168a5e264655f004868a6ba0343304c08801c05b469eaeb89a6b03fa72ec143c8639'
